f71058219bb50f6f4df625284d13ee36d4da9063
macmanuel08 <manueljm016@gmail.com>
2021-07-05 09:50:50 +0800

    final weather site 2nd draft

--- WDD230/lesson11/js/event.js
+++ WDD230/lesson11/js/event.js
@@ -1,5 +1,6 @@
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
+
 fetch(requestURL)
     .then(function (response) {
         return response.json();
@@ -12,77 +13,16 @@ fetch(requestURL)
         const needTowns = towns.filter(town => town.name == 'Preston' || town.name == 'Soda Springs' || town.name == 'Fish Haven');
         // console.table(needTowns);
 
-        const eventPreston = document.querySelector('#eventPreston');
-        const preston = "Preston";
+        const event = document.querySelector('#events');
  
         needTowns.forEach(town => {
 
-          if (town.name == preston) {
+          if (town.name == cityName) {
             for (let i = 0; i < town.events.length; i++) {
-              let ep = document.createElement('p');
+              let p = document.createElement('p');
 
-              ep.textContent = town.events[i];
-              eventPreston.appendChild(ep);
-            }
-          }
-
-          
-        });
-    });
-
-    fetch(requestURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (jsonObject) {
-        //console.table(jsonObject);
-
-        const towns = jsonObject['towns'];
-
-        const needTowns = towns.filter(town => town.name == 'Preston' || town.name == 'Soda Springs' || town.name == 'Fish Haven');
-
-        const eventSoda = document.querySelector('#eventSoda');
-
-        const soda = "Soda Springs";
-        
-        needTowns.forEach(town => {
-
-          if (town.name == soda) {
-            for (let i = 0; i < town.events.length; i++) {
-              let es = document.createElement('p');
-
-              es.textContent = town.events[i];
-              eventSoda.appendChild(es);
-            }
-          }
-
-          
-        });
-    });
-
-    fetch(requestURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (jsonObject) {
-        //console.table(jsonObject);
-
-        const towns = jsonObject['towns'];
-
-        const needTowns = towns.filter(town => town.name == 'Preston' || town.name == 'Soda Springs' || town.name == 'Fish Haven');
-
-        const eventFish = document.querySelector('#eventFish');
-
-        const fish = "Fish Haven";
-        
-        needTowns.forEach(town => {
-
-          if (town.name == fish) {
-            for (let i = 0; i < town.events.length; i++) {
-              let ef = document.createElement('p');
-
-              ef.textContent = town.events[i];
-              eventFish.appendChild(ef);
+              p.textContent = town.events[i];
+              event.appendChild(p);
             }
           }
 
--- WDD230/lesson11/js/responsiveMenu.js
+++ WDD230/lesson11/js/responsiveMenu.js
@@ -98,12 +98,10 @@ const diffInTime = date - lastVisitDay;
 const oneDay = 1000 * 60 * 60 * 24;
 const diffInDays = Math.ceil(diffInTime / oneDay);
 
-if (diffInDays < 2) {
-    if (diffInDays == 1) {
-        document.querySelector("#visit").textContent = "Last visit: 1 Day Ago";
-    }
+if (lastVisitDay >= 1 && diffInDays >= 1) {
+    document.querySelector("#visit").textContent = `Last visit: ${diffInDays} Day(s) Ago`;
 } else {
-    document.querySelector("#visit").textContent = `Last visit: ${diffInDays} Days Ago`;
+    document.querySelector("#visit").textContent = "This is your first visit";
 }
 
 localStorage.setItem("lastVisit", date.toISOString());
