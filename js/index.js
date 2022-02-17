4ead569ee548b6084402df2f52665e0be0d78b49
John Mark Manuel <manueljm016@gmail.com>
2022-02-17 11:59:13 +0800

    transition added to the about section

--- js/index.js
+++ js/index.js
@@ -11,46 +11,18 @@ function toggleNav() {
   document.getElementById("navigation").classList.toggle("display");
 }
 
-/* Slide In */
+/* Slide In Animation Using Intersection Observer */
 
-// Getting all images with data-src attribute
-const imagesToLoad = document.querySelectorAll("img[data-src]");
+const contents = document.querySelectorAll('#about > *');
 
-const loadImages = (image) => {
-    //get 'src' attr and set its value to 'data-src' value
-    image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => {
-        image.removeAttribute('data-src');
-    };
-};
-
-// optional parameters being set for the IntersectionObserver
-const imgOptions = {
-    threshold: 1,
-    rootMargin: "0px 0px 50px 0px"
-};
-
-if ('IntersectionObserver' in window) {
-    // if it's supported
-    const imgObserver = new IntersectionObserver(items => {
-        items.forEach(item => {
-            if (item.isIntersecting) {
-                loadImages(item.target);
-                imgObserver.unobserve(item.target);
-            }
-        });
-    }, imgOptions);
-
-    //load images if necessary
-    imagesToLoad.forEach(img => {
-        imgObserver.observe(img);
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle('show', entry.isIntersecting);
     });
+});
 
-} else { //load  all images if not supported
-    imagesToLoad.forEach(img => {
-        loadImages(img);
-    });
-}
+contents.forEach(content => observer.observe(content));
+
 
 /* Projects DOM */
 
@@ -98,8 +70,8 @@ fetch("js/projects.json")
 
 /* FORM */
 
-function submit() {
-    document.querySelector('.message-status').innerHTML = 'Message Sent!';
+// function submit() {
+//     document.querySelector('.message-status').innerHTML = 'Message Sent!';
 
-    return true;
-}
+//     return true;
+// }
